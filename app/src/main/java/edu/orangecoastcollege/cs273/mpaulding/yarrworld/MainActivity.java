269e532f89fe8198393282454e0e3e3639086c9e
mpaulding <mpaulding@occ.cccd.edu>
2016-08-24 14:22:31 -0700

    Modified the speakButton to use an event listener, with anonymous inner class.

--- app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/yarrworld/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/yarrworld/MainActivity.java
@@ -2,28 +2,40 @@ package edu.orangecoastcollege.cs273.mpaulding.yarrworld;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     final Context context = this;
+    private Button speakButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        speakButton =  (Button) findViewById(R.id.speakButton);
+        speakButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(
+                                context, R.string.alert_dialog_message, Toast.LENGTH_SHORT ).show();
+//                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+//                        alertDialogBuilder.setTitle(getString(R.string.alert_dialog_title));
+//                        alertDialogBuilder.setMessage(getString(R.string.alert_dialog_message));
+//                        alertDialogBuilder.setCancelable(true);
+//
+//                        AlertDialog alertDialog = alertDialogBuilder.create();
+//                        alertDialog.show();
+                    }
+                }
+        );
+
     }
 
-    public void speakPirate(View button)
-    {
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
-        alertDialogBuilder.setTitle(getString(R.string.alert_dialog_title));
-        alertDialogBuilder.setMessage(getString(R.string.alert_dialog_message));
-        alertDialogBuilder.setCancelable(true);
 
-        AlertDialog alertDialog = alertDialogBuilder.create();
-        alertDialog.show();
-    }
 }
